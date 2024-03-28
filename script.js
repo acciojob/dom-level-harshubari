74f9de21de2e60380bcbe146f4d32061ba04bf2a
harshubari <121845828+harshubari@users.noreply.github.com>
2024-03-28 09:19:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+function findDomLevel() {
+    var levelElement = document.getElementById('level');
+    var domLevel = 0;
+    while (levelElement) {
+        domLevel++;
+        levelElement = levelElement.parentElement;
+    }
+    alert('The level of the element is: ' + domLevel);
+}
+
+
+findDomLevel();
